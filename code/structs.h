73be7fba380800eeea6db04d1ff0726028716c6b
AndrewSpano <49786297+AndrewSpano@users.noreply.github.com>
2020-01-14 14:10:24 +0200

    Add files via upload

---
+++ code/structs.h
@@ -0,0 +1,72 @@
+#ifndef __STRUCTS__
+#define __STRUCTS__
+
+#define MAX_HOLES 1000
+
+typedef unsigned int uint;
+typedef unsigned char byte;
+typedef unsigned long long ull;
+
+typedef struct hole
+{
+  ull start;
+  ull end;
+} hole;
+
+
+typedef struct hole_table
+{
+  hole holes[MAX_HOLES];
+} hole_table;
+
+
+typedef struct
+{
+  uint id;
+  char name[];
+  uint size;
+  uint type;
+  ull parent_id;
+  time_t creation_time;
+  time_t access_time;
+  time_t modification_time;
+
+  uint blocks_using;
+  ull first_block;
+
+} MDS;
+
+
+
+typedef struct block
+{
+  ull next_block;
+  byte data[];
+} block;
+
+
+
+typedef struct superblock
+{
+  uint files;
+  uint links;
+  uint directories;
+
+  ull root_directory;
+  uint current_size;
+
+  uint current_hole_number;
+
+  uint block_size;
+  uint filename_size;
+  uint max_file_size;
+  uint max_dir_file_number;
+
+  char cfs_filename[];
+
+} superblock;
+
+
+
+
+#endif
