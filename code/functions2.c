1e32bb68d395907ad86d508ce9596b921d8b6780
dimpapac <demispapa@gmail.com>
2020-01-20 03:59:24 +0200

    fun2

--- code/functions2.c
+++ code/functions2.c
@@ -47,6 +47,7 @@ off_t find_hole(char* cfs_filename, int fd, size_t my_size)
     {
       offset_to_return = holes->holes_table[i].start;
       holes->holes_table[i].start = holes->holes_table[i].end = 0; //clear hole
+      //holes->current_hole_number--; //after shifting
       free(holes);
       return offset_to_return;
     } else if (available_size > my_size) //leaves a hole
@@ -65,37 +66,69 @@ off_t find_hole(char* cfs_filename, int fd, size_t my_size)
 
 }
 
-int cfs_mkdir(char * name, uint bs, uint fns, uint cfs, uint mdfn, int fd)
+int cfs_mkdir(char * name, uint bs, uint fns, uint cfs, uint mdfn, int fd, Stack_List* list)
 {
-  //find holes for mds and data block 
-  // off_t offset_for_mds = find_hole(name, fd, sizeof(MDS));
-  // off_t offset_for_db = find_hole(name, fd, bs);
+  //need to check if #_of_entities< mdfn
 
-  // //initialize data block
-  // Block* db = NULL;
-  // MALLOC_OR_DIE(root_data, block_size, fd);
-  // /* initialize its values */
+
+  // find holes for mds and data block 
+  off_t offset_for_mds = find_hole(name, fd, sizeof(MDS));
+  off_t offset_for_db = find_hole(name, fd, bs);
+
+  //initialize data block
+  Block* db = NULL;
+  MALLOC_OR_DIE(db, bs, fd);
+  /* initialize its values */
+
+  //get current directory offset
+  char * parent_name = malloc(sizeof(char) * 30);
+  off_t parent_offset;
+  Stack_List_Peek(list, &parent_name, &parent_offset);
+
+
   // void initialize_Directory_Data_Block(Block* block, size_t fns, off_t self_offset, off_t parent_offset);
-  // initialize_Directory_Data_Block(db, fns, /*self_offset*/, /*parent_offset*/);
-  // /* write to the cfs file */
-  // WRITE_OR_DIE(fd, root_data, block_size);
+  initialize_Directory_Data_Block(db, fns, offset_for_db, parent_offset);
+   // write to the cfs file 
+  WRITE_OR_DIE(fd, db, bs);
 
-  // //initialize mds
-  // /* create the struct */
-  // MDS* mds = NULL;
-  // MALLOC_OR_DIE(mds, sizeof(MDS), fd);
-  // /* initialize its values */
+  //initialize mds
+  //get superblock or id 
+  superblock* sup = get_superblock(fd);
+
+  /* create the struct */
+  MDS* mds = NULL;
+  MALLOC_OR_DIE(mds, sizeof(MDS), fd);
+  /* initialize its values */
   // void initialize_MDS(MDS* mds, uint id, uint type, uint number_of_hard_links, uint blocks_using, size_t size, off_t parent_offset, off_t first_block);
-  // initialize_MDS(mds, /*3*/, DIRECTORY, 1, 1, mds_size + bs, /*parent offset from list*/, offset_for_db);
-  // /* write to the cfs file */
-  // WRITE_OR_DIE(fd, mds, mds_size);
-
-
-
+  initialize_MDS(mds, sup->total_entities + 1, DIRECTORY, 1, 1, sizeof(MDS) + bs, parent_offset, offset_for_db);
+  /* write to the cfs file */
+  WRITE_OR_DIE(fd, mds, sizeof(MDS));
 
   //enimerosi parent directory
+  off_t current_offset = parent_offset; 
+  MDS* mds_cur = get_MDS(fd, parent_offset);
+  Block* last_block = get_Block(fd, bs, mds_cur->first_block);
+  while (last_block->next_block != 0)
+  {
+    last_block = get_Block(fd, bs, last_block->next_block);
+  }
+  
+  if(directory_data_block_Is_Full(last_block, bs, fns) == 0) //there is space in the block
+  {  
+    insert_pair(last_block, name, offset_for_mds, fns);
+  }
+  else //block is full
+  { //allocate new block
+
+  }
+
+
+
 
   //enimerosi superblock
+  sup->total_entities += 1;
+  sup->current_size += sizeof(MDS) + bs; //parent's block had space for the new entry 
+
   return 0;
 } 
 
--- code/functions2.h
+++ code/functions2.h
@@ -1,6 +1,11 @@
 #ifndef __FUNCTIONS2__
 #define __FUNCTIONS2__
 
+#include "structs.h"
+#include "macros.h"
+#include "list.h"
+
 off_t find_hole(char* filename, int fd, size_t my_size);
+int cfs_mkdir(char * name, uint bs, uint fns, uint cfs, uint mdfn, int fd, Stack_List* list);
 
 #endif
